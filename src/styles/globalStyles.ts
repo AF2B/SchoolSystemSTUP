b1efe745ee1228fe94c63c8a143b62ca5659fefa
andre.borba <dev_andre.filipe@outlook.com>
2023-08-22 00:34:40 -0300

    Configured global styles with Redux

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    body {
+        background: ${({ theme }) => theme.bg};
+        color: ${({ theme }) => theme.text};
+        font-family: 'Roboto', sans-serif;
+        letter-spacing: .6px;
+    }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+export const lightTheme = {
+  text: '#FFFFFF',
+  text2: '#000000',
+  textInvert: '#A3A3A3',
+  textInvert2: '#14238A',
+  primary: '#14238A',
+  primary2: '#131F73',
+  bg: '#E5E5E5',
+  bg2: '#FFFFFF',
+};
+
+export const darkTheme = {
+  text: '#fff',
+  text2: '#f0f0f0',
+  textInvert: '#333',
+  textInvert2: '#555',
+  primary: '#3498db',
+  primary2: '#85c1e9',
+  bg: '#2c3e50',
+  bg2: '#34495e',
+};
