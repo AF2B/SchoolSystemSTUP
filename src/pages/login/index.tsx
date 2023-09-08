113ec51803047c8cc1a6f86194ce7a7a9f591b93
alexsouza10 <alexsouza160298@gmail.com>
2023-09-08 20:54:05 -0300

    email validation

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,54 +1,88 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { 
-  FlexBox, 
+  Container, 
   LoginForm, 
   Input, 
   Button,
   Password, 
-  P
+  ErrorMessage
 } from './style';
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const usernameRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
   const [errorMessage, setErrorMessage] = useState<string>('');
 
-  const handleLogin = () => {
-    if (username === 'admin' && password === 'admin') {
-      console.log('Login successful!');
-      window.location.href = 'https://www.google.com'; // Alterar o redirecionamento
-    } else {
-      setErrorMessage('Invalid username or password');
+  const handleLogin = async () => {
+    const usernameValue = usernameRef.current?.value || '';
+    const passwordValue = passwordRef.current?.value || '';
+
+    // Regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(usernameValue)) {
+      setErrorMessage('Invalid email address');
+      return; // Return early if email is invalid
+    }
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: usernameValue, password: passwordValue }),
+      });
+
+      if (response.ok) {
+        console.log('Login successful!');
+        window.location.href = 'google.com.br'; // Página de redirecionamento após o login bem-sucedido
+      } else {
+        setErrorMessage('Invalid username or password');
+        resetFields(); // Chama a função para limpar os campos
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setErrorMessage('An error occurred while logging in.');
+    }
+  };
+
+  const resetFields = () => {
+    if (usernameRef.current) {
+      usernameRef.current.value = '';
+    }
+    if (passwordRef.current) {
+      passwordRef.current.value = '';
     }
   };
 
   const handleForgoutPassword = () => {
-    window.location.href = 'https://www.google.com'; // Alterar o redirecionamento
+    window.location.href = 'google.com.br'; // Página de redirecionamento
   };
 
   return (
-    <FlexBox>
+    <Container>
       <LoginForm>
-        {errorMessage && <P>{errorMessage}</P>}
-        <Input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Password>
-          <h4 onClick={handleForgoutPassword}>Forgot your password?</h4>
-        </Password>
-        <Button onClick={handleLogin}>SIGN IN</Button>
+        <ErrorMessage>
+          {errorMessage}
+          <Input
+            type="text"
+            placeholder="Email"
+            ref={usernameRef}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+          />
+          <Password>
+            <h4 onClick={handleForgoutPassword}>Forgot your password?</h4>
+          </Password>
+          <Button onClick={handleLogin}>SIGN IN</Button>
+        </ErrorMessage>
       </LoginForm>
-    </FlexBox>
+    </Container>
   );
 };
 
